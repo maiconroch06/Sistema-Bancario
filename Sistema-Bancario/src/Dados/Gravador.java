377a8440a50fbbaedc72e1a8f043fcd1e6b873f7
Maicon_Rocha <maiconpablo588@gmail.com>
2026-02-12 21:35:30 -0300

    feat: salvamento e leitura dos dados em txt

--- Sistema-Bancario/src/Dados/Gravador.java
+++ Sistema-Bancario/src/Dados/Gravador.java
@@ -1,9 +1,11 @@
-package Dados;
+package dados;
 
+import classes.ContaBancaria;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -12,67 +14,46 @@ public class Gravador {
     
     private File pasta;
     private File arquivo;
+    
+    HashMap<String, ContaBancaria> linhas;
 
-    public Gravador() {
-        pasta = new File(System.getProperty("user.dir"), "dados");
+    public Gravador(HashMap<String, ContaBancaria> contas) {
+        pasta = new File(System.getProperty("user.dir"), "src\\dados");
         pasta.mkdirs();
-
+        
         arquivo = new File(pasta, "contas.txt");
+
+        this.linhas = contas;
     }
 
-    public void escritaEmArquivo(String texto){
-
+    public void escritaEmArquivo(){
+            
         try {
+            FileWriter escrita = new FileWriter(arquivo);
+            PrintWriter saida = new PrintWriter(escrita);
+            
+            String cabecario = "numero,titutar,saldo,tipo,caracteristica";
+            saida.println(cabecario);
+            
+            for(ContaBancaria conta : linhas.values()) {
+                
+                String numero = conta.getNumero();
+                String titular = conta.getNome();
+                String saldo = String.valueOf(conta.getSaldo());
+                String tipo = conta.getTipoConta();
+                String caractristica = String.valueOf(conta.getInformacao());
+                
+                String linha = numero + "," + titular + "," + saldo + "," + tipo + "," + caractristica;
+                
+                saida.println(linha);
 
-          FileWriter escrita = new FileWriter(arquivo);
-          PrintWriter saida = new PrintWriter(escrita);
-
-          saida.write(texto);
-
-          saida.close();
-          escrita.close();
-
-        } catch (IOException ex) {
-          //Logger.getLogger(Arquivos.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-      }
-
-    public void criarDiretorios(){
-
-        pasta = new File("C:\\\\Dados\\\\contas.txt");
-
-        if(pasta.mkdirs()){
-          JOptionPane.showMessageDialog(null, "Diretório criado com sucesso");
-        }else{
-          if(pasta.exists()){
-            JOptionPane.showMessageDialog(null, "O diretório já existe");
-          }else{
-            JOptionPane.showMessageDialog(null, "O diretório não foi criado");
-          }
-        }
-      }
-
-      public void criarArquivo(){
-
-        arquivo = new File(pasta,"teste.txt");
-
-        try {
-
-          if(arquivo.createNewFile()){
-            JOptionPane.showMessageDialog(null, "arquivo criado com sucesso");
-          }else{
-            if(arquivo.exists()){
-              JOptionPane.showMessageDialog(null, "O arquivo já existe");
-            }else{
-              JOptionPane.showMessageDialog(null, "O arquivo não foi criado");
             }
-          }
 
+            saida.close();
+            escrita.close();
         } catch (IOException ex) {
-          //Logger.getLogger(Arquivos.class.getName()).log(Level.SEVERE, null, ex);
+          Logger.getLogger(Gravador.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-      }
+    }
     
 }
--- Sistema-Bancario/src/Dados/Leitor.java
+++ Sistema-Bancario/src/Dados/Leitor.java
@@ -1,35 +1,102 @@
-package Dados;
+package dados;
 
-import java.io.BufferedReader;
+import classes.ContaBancaria;
+import classes.ContaCorrente;
+import classes.ContaPoupanca;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Leitor {
 
-    File arquivo = new File("C:Dados/contas.txt");
+    File pasta;
+    File arquivo;
+    
+    HashMap<String, ContaBancaria> linhas;
 
-    public void lerArquivo() {
-        try {
-            FileReader leitor = new FileReader(arquivo);
-            BufferedReader br = new BufferedReader(leitor);
-
-            String s;
-
-        try {
-            while((s = br.readLine()) != null){
-                System.out.println(s);
-            }
-        } catch (IOException ex) {
-        Logger.getLogger(Leitor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        } catch (FileNotFoundException ex) {
-        Logger.getLogger(Leitor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public Leitor(HashMap<String, ContaBancaria> contas) {
+        pasta = new File(System.getProperty("user.dir"), "src\\dados");
+        pasta.mkdirs();
+        
+        arquivo = new File(pasta, "contas.txt");
+        this.linhas = contas;
     }
 
+    public HashMap<String, ContaBancaria> lerArquivo() {
+        try {
+            Scanner leitor = new Scanner(arquivo);
+            leitor.nextLine();
+            
+            while(leitor.hasNextLine()) {
+                String linha = leitor.nextLine();
+                String[] conteudo = linha.split(",");
+                
+                if (conteudo.length < 5) {
+                    System.out.println("Linha inválida: " + linha);
+                    continue;
+                }
+                
+                String numero = conteudo[0];
+                String titular = conteudo[1];
+                double saldo = Double.parseDouble(conteudo[2]);
+                String tipo = conteudo[3];
+                double extra = Double.parseDouble(conteudo[4]);
+                
+                ContaBancaria conta;
+                
+                if(tipo.equalsIgnoreCase("CORRENTE")) {
+                    conta = new ContaCorrente(numero, titular, saldo, tipo, extra);
+                
+                } else if(tipo.equalsIgnoreCase("POUPANÇA")) {
+                    conta = new ContaPoupanca(numero, titular, saldo, tipo, extra);
+                
+                } else {
+                    System.out.println("Tipo inválido - Conta:" + numero + " Tipo: " + tipo);
+                    continue;
+                }
+                linhas.put(numero, conta);
+            }
+            
+            leitor.close();
+            return linhas;
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Leitor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NumberFormatException ex) {
+            System.out.println("Erro na conversão numérica");
+        }
+        return linhas;   
+    }
+    
 }
+
+
+//public void lerArquivo() {
+//    try {
+//        Scanner leitor = new Scanner(arquivo);
+//
+//        while(leitor.hasNextLine()) {
+//            String linha = leitor.nextLine();
+//            linhas.add(linha.split(","));
+//        }
+//
+//        for (String[] conteudo : linhas) {
+//            for (int i = 0; i < conteudo.length; i++) {
+//                System.out.print(conteudo[i]);
+//
+//                if (i < conteudo.length - 1) {
+//                    System.out.print(" - ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//
+//        leitor.close();
+//
+//    } catch (FileNotFoundException e) {
+//        Logger.getLogger(Leitor.class.getName()).log(Level.SEVERE, null, e);
+//    }
+//}
